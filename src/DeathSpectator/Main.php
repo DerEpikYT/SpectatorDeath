0ff0104d8f8ff62a04f6b1e32c738a4953b98525
DerEpikYT <derepik3@gmail.com>
2025-04-11 19:45:50 +0200

    Added the plugin files.

---
+++ src/DeathSpectator/Main.php
@@ -0,0 +1,32 @@
+<?php
+namespace myplugin;
+
+use DeathSpectator\RespawnCountdownTask;
+use pocketmine\player\GameMode;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable(): void {
+        $this->saveDefaultConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onPlayerDeath(PlayerDeathEvent $event): void {
+        $player = $event->getPlayer();
+        $playerName = $player->getName();
+
+        $messageTemplate = $this->getConfig()->get("deathmessage");
+        $customMessage = str_replace("$player", $playerName, $messageTemplate);
+
+        $event->setDeathMessage($customMessage);
+        $respawnTime = 5;
+        $player->setGamemode(GameMode::SPECTATOR());
+
+        // Countdown starten
+        $gamemode = $this->getConfig()->get("respawngamemode");
+        $this->getScheduler()->scheduleRepeatingTask(new RespawnCountdownTask($player, $respawnTime, $gamemode), 20);
+    }
+}
---
+++ src/DeathSpectator/RespawnCountdownTask.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace DeathSpectator;
+
+use pocketmine\player\GameMode;
+use pocketmine\scheduler\Task;
+use pocketmine\player\Player;
+use pocketmine\utils\Config;
+use pocketmine\world\sound\NoteInstrument;
+use pocketmine\world\sound\NoteSound;
+use pocketmine\world\sound\XpCollectSound;
+
+class RespawnCountdownTask extends Task {
+    private Player $player;
+    private int $secondsLeft;
+
+    private string $gamemode;
+
+    public function __construct(Player $player, int $seconds, string $gamemode) {
+        $this->player = $player;
+        $this->secondsLeft = $seconds;
+        $this->gamemode = strtolower($gamemode);
+    }
+
+    public function onRun(): void {
+        if (!$this->player->isOnline()) {
+            $this->getHandler()?->cancel();
+            return;
+        }
+
+        if ($this->secondsLeft > 0) {
+            $this->player->getWorld()->addSound($this->player->getPosition(), new XpCollectSound());
+            $this->player->sendTitle("§eYou will respawn in §c{$this->secondsLeft}...");
+            $this->secondsLeft--;
+        } else {
+            $this->player->sendMessage("Respawning...");
+            switch ($this->gamemode) {
+                case ("creative" or "CREATIVE"):
+                    $this->player->setGamemode(GameMode::CREATIVE());
+                    break;
+                case ("adventure" or "ADVENTURE"):
+                    $this->player->setGamemode(GameMode::ADVENTURE());
+                    break;
+                case ("survival" or "SURVIVAL"):
+                    $this->player->setGamemode(GameMode::SURVIVAL());
+                    break;
+                default:
+                    $this->player->setGamemode(GameMode::SURVIVAL());
+                    break;
+            }
+            $this->player->teleport($this->player->getWorld()->getSafeSpawn());
+            $this->getHandler()?->cancel();
+        }
+    }
+}
